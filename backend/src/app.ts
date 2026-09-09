1ce5e9fca4540847717e3ce6aa76bd96c21f01c5
chika <chika.yamamoto95@gmail.com>
2026-09-09 23:33:03 +0900

    feat:7章　データベース連携

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { query, closePool } from './db';
 
 // 型をインポートする
 import type { Request, Response } from 'express';
 
+// ToDoの型をインターフェースとして定義する
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}
 // .envファイルを読み込む
 dotenv.config();
 
@@ -16,30 +24,43 @@ const app = express();
 
 // CORSの設定を行う
 app.use(cors({
- origin: 'http://localhost:5173',  // 許可するオリジン
- methods: ['GET']                  // 許可するHTTPメソッド
+  origin: 'http://localhost:5173',  // 許可するオリジン
+  methods: ['GET']                  // 許可するHTTPメソッド
 }));
 
-// ToDoの全データ
-const todos = [
- { id: 1, title: 'Reactを勉強する', completed: true, createdAt: new Date() },
- { id: 2, title: 'Node.jsを勉強する', completed: true, createdAt: new Date() },
- { id: 3, title: 'ToDoアプリを作る', completed: false, createdAt: new Date() },
-];
-
+// サーバーエラーを処理する関数
+function handleServerError(res: Response, err: unknown, message: string = 'サーバーエラー') {
+  console.error(err);
+  res.status(500).json({ error: message });
+}
 // ToDoの全データを返すルート
-app.get('/api/todos', (req: Request, res: Response) => {
- // ToDoの全データをJSON形式に変換して返す
- res.json(todos);
+app.get('/api/todos', async (req: Request, res: Response) => {
+  try {
+    const sql = 'SELECT id, title, completed, created_at AS createdAt FROM todos ORDER BY createdAt DESC';
+    const rows = await query<Todo>(sql);
+
+    res.status(200).json(rows);
+  } catch (err) {
+    handleServerError(res, err);
+  }
 });
 
 // 定義したルート以外へのアクセスに対する処理（404 Not Found）
 app.use((req: Request, res: Response) => {
- res.status(404).set('Content-Type', 'text/html; charset=utf-8');
- res.send('<h1>ページが見つかりませんでした。</h1>');
+  res.status(404).set('Content-Type', 'text/html; charset=utf-8');
+  res.send('<h1>ページが見つかりませんでした。</h1>');
 });
+
+// アプリ終了時にDB接続プールを安全に破棄する
+['SIGINT', 'SIGTERM', 'SIGHUP'].forEach(signal => {
+  process.on(signal, async () => {
+    console.log(`\n${signal}を受信。アプリケーションの終了処理中...`);
+    await closePool();
+    process.exit();
+  });
+})
 
 // Webサーバーを指定したポートで起動する
 app.listen(port, () => {
- console.log(`Webサーバーが起動しました。`);
+  console.log(`Webサーバーが起動しました。`);
 });
