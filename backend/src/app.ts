bd8dfef082304aa33a5024311176965eda744c58
chika <chika.yamamoto95@gmail.com>
2026-09-10 20:04:55 +0900

    feat:12章　リファクタリング

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { query, exec, closePool } from './db';
+import { closePool } from './db';
+import todoRoutes from './routes/todo';
 
 // 型をインポートする
 import type { Request, Response } from 'express';
 
-// ToDoの型をインターフェースとして定義する
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-  createdAt: Date;
-}
+
 // .envファイルを読み込む
 dotenv.config();
 
@@ -31,92 +26,9 @@ app.use(cors({
 // JSON形式のリクエストボディを解析するミドルウェアを追加する
 app.use(express.json());
 
-// サーバーエラーを処理する関数
-function handleServerError(res: Response, err: unknown, message: string = 'サーバーエラー') {
-  console.error(err);
-  res.status(500).json({ error: message });
-}
-// ToDoの全データを返すルート
-app.get('/api/todos', async (req: Request, res: Response) => {
-  try {
-    const sql = 'SELECT id, title, completed, created_at AS createdAt FROM todos ORDER BY createdAt DESC';
-    const rows = await query<Todo>(sql);
+// ToDoのCRUD機能を担当する各ルートを読み込む
+app.use('/api/todos', todoRoutes);
 
-    res.status(200).json(rows);
-  } catch (err) {
-    handleServerError(res, err);
-  }
-});
-// ToDoを追加するルート
-app.post('/api/todos', async (req: Request, res: Response) => {
-  const { title }: { title: string } = req.body;
-
-  if (!title.trim()) {
-    res.status(400).json({ error: 'ToDoを入力してください。' });
-    return;
-  }
-  if (title.trim().length > 50) {
-    res.status(400).json({ error: 'ToDoは50文字以内で入力してください。' });
-    return;
-  }
-
-  try {
-    const sql = 'INSERT INTO todos (title, completed, created_at) VALUES (?, ?, ?)';
-    const params = [title, false, new Date()];
-
-    await exec(sql, params);
-
-    res.status(201).json({ message: 'ToDoを追加しました。' });
-  } catch (err) {
-    handleServerError(res, err);
-  }
-});
-// ToDoを更新するルート
-app.put('/api/todos/:id', async (req: Request, res: Response) => {
-  const { title, completed }: { title: string, completed: boolean } = req.body;
-
-  if (!title.trim()) {
-    res.status(400).json({ error: 'ToDoを入力してください。' });
-    return;
-  }
-
-  if (title.trim().length > 50) {
-    res.status(400).json({ error: 'ToDoは50文字以内で入力してください。' });
-    return;
-  }
-
-  try {
-    const sql = 'UPDATE todos SET title = ?, completed = ? WHERE id = ?';
-    const params = [title, completed, req.params.id];
-    const result = await exec(sql, params);
-
-    if (result.affectedRows === 0) {
-      res.status(404).json({ error: '指定されたToDoが見つかりません。' });
-      return;
-    }
-
-    res.status(200).json({ message: 'ToDoを更新しました。' });
-  } catch (err) {
-    handleServerError(res, err);
-  }
-});
-// ToDoを削除するルート
-app.delete('/api/todos/:id', async (req: Request, res: Response) => {
-  try {
-    const sql = 'DELETE FROM todos WHERE id = ?';
-    const params = [req.params.id];
-    const result = await exec(sql, params);
-
-    if (result.affectedRows === 0) {
-      res.status(404).json({ error: '指定されたToDoが見つかりません。' });
-      return;
-    }
-
-    res.status(200).json({ message: 'ToDoを削除しました。' });
-  } catch (err) {
-    handleServerError(res, err);
-  }
-});
 // 定義したルート以外へのアクセスに対する処理（404 Not Found）
 app.use((req: Request, res: Response) => {
   res.status(404).set('Content-Type', 'text/html; charset=utf-8');
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import TodoForm from './components/TodoForm';
+import { fetchTodos, addTodo, updateTodo, deleteTodo } from './api/todo';
+
 // ToDoの型をインターフェースとして定義する
 interface Todo {
   id: number;
@@ -9,16 +11,8 @@ interface Todo {
 }
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const apiUrl = import.meta.env.VITE_API_URL;
   const [editingId, setEditingId] = useState<number | null>(null);
-  // APIにリクエストを送信し、ToDo一覧を取得する関数
-  async function fetchTodos(): Promise<Todo[]> {
-    const res = await fetch(`${apiUrl}/todos`);
 
-    if (!res.ok) throw new Error('ToDo一覧の取得に失敗しました。');
-
-    return res.json();
-  };
 
   // ToDo一覧を取得し、状態変数を更新する関数
   async function syncTodos() {
@@ -50,24 +44,7 @@ function App() {
 
     if (!res.ok) throw new Error('ToDoの追加に失敗しました。');
   };
-  // APIにリクエストを送信し、ToDoを更新する関数
-  async function updateTodo(id: number, title: string, completed: boolean) {
-    const res = await fetch(`${apiUrl}/todos/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title, completed }),
-    });
 
-    if (!res.ok) throw new Error('ToDoの更新に失敗しました。');
-  };
-  // APIにリクエストを送信し、ToDoを削除する関数
-  async function deleteTodo(id: number) {
-    const res = await fetch(`${apiUrl}/todos/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (!res.ok) throw new Error('ToDoの削除に失敗しました。');
-  };
   return (
     <>
       <h2>ToDo一覧</h2>
