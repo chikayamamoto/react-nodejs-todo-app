1d76caf075afa7c3f938d616340b096fe5ba0633
chika <chika.yamamoto95@gmail.com>
2026-09-11 20:26:57 +0900

    feat:17章　CSS追加

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,35 +1,46 @@
+import { UserPlus, LogIn, LogOut } from 'lucide-react';
+
 type Props = {
- isLoggedIn: boolean;
- onClickLogout: () => void;
- onClickLogin: () => void;
- onClickRegister: () => void;
+  isLoggedIn: boolean;
+  onClickLogout: () => void;
+  onClickLogin: () => void;
+  onClickRegister: () => void;
 };
 
 function Header({ isLoggedIn, onClickLogout, onClickLogin, onClickRegister }: Props) {
- return (
-   <header>
-     <nav>
-       <h1>ToDoアプリ</h1>
+  return (
+    <header>
+      <nav>
+        <h1>ToDoアプリ</h1>
 
-       <ul>
-         {isLoggedIn ? (
-           <li>
-             <button onClick={onClickLogout}>ログアウト</button>
-           </li>
-         ) : (
-           <>
-             <li>
-               <button onClick={onClickLogin}>ログイン</button>
-             </li>
-             <li>
-               <button onClick={onClickRegister}>会員登録</button>
-             </li>
-           </>
-         )}
-       </ul>
-     </nav>
-   </header>
- );
+        <ul>
+          {isLoggedIn ? (
+            <li>
+              <button onClick={onClickLogout}>
+                <LogOut />
+                <span>ログアウト</span>
+              </button>
+            </li>
+          ) : (
+            <>
+              <li>
+                <button onClick={onClickLogin}>
+                  <LogIn />
+                  <span>ログイン</span>
+                </button>
+              </li>
+              <li>
+                <button onClick={onClickRegister}>
+                  <UserPlus />
+                  <span>会員登録</span>
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
